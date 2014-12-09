328afd1715091c88acdc1fdada85ffd9b46b3563
emjsmonson/em <emjsmonson@gmail.com/em>
2014-12-09 15:13:43 -0700

    Updated EnvironmentPkg.sv

--- EnvironmentPkg.sv
+++ EnvironmentPkg.sv
@@ -1,6 +1,13 @@
 package EnvironmentPkg;
 
-`include "Instruction.sv"
-`include "ScoreBoard.sv"
+enum { BR, ADD, LD, ST,
+       JSR, AND, LDR, STR,
+       RTI, NOT, LDI, STI,
+       JMP, RES, LEA, TRAP
+     } Opcodes; 
    
-endpackage
+`include "MemoryTransaction.sv"
+`include "Scoreboard.sv"
+   
+endpackage // EnvironmentPkg
+   
