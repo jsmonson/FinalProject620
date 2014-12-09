51392fa44dd1761b3edf6d9cee0523ae208dc311
Aaron Stoddard <aaron1stoddard@yahoo.com>
2014-12-09 15:40:34 -0700

    classes

--- EnvironmentPkg.sv
+++ EnvironmentPkg.sv
@@ -18,39 +18,7 @@ enum { BR, ADD, LD, ST,
 `include "Generator.sv"
 `include "Agent.sv"
 `include "Driver.sv"
+`include "Environment.sv"
 
-
-  
-   
-  
-  
-  // Will have to merge our environments...
-   class Environment;
-    Generator gen;
-    Agent agt;
-    Driver drv;
-    mailbox #(MemoryTransaction) gen2agt, agt2drv, agt2scb;
-    int count;
-    event gen2agths, agt2drvhs, agt2scbhs, chk2gen;
-    function new(int count);
-       this.count = count;
-    endfunction;
-    function void build();
-      gen2agt = new(1);
-      agt2drv = new(1);
-	  agt2scb = new(1);
-      gen = new(gen2agt,gen2agths, chk2gen);
-      agt = new(gen2agt,agt2drv, agt2scb, gen2agths,agt2drvhs,agt2scbhs);
-      drv = new(agt2drv,agt2drvhs);
-    endfunction
-  
-    task run();
-        fork
-          gen.run(count);
-          agt.run(count);
-          drv.run(count);
-        join
-     endtask  
-  endclass
 endpackage // EnvironmentPkg
    
